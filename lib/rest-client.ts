0ca219c77cde34ed5db9e679211bf6a33a27d9e8
Mehrdad Youssefi <themehrdad@gmail.com>
2019-12-15 23:56:38 -0800

    added tests and method (#6)

--- lib/rest-client.ts
+++ lib/rest-client.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import {IBaseUrl} from "./base-urls/base-url-interfaces";
-import {IEloquaCredentials} from "./eloqua-credentials";
-import { IListRequestOptions, IListResponse } from "./rest-api-interfaces";
+import { IBaseUrl } from "./base-urls/base-url-interfaces";
+import { IEloquaCredentials } from "./eloqua-credentials";
+import { Depth, IListRequestOptions, IListResponse } from "./rest-api-interfaces";
 
 export class RestClient {
   constructor(
@@ -24,7 +24,23 @@ export class RestClient {
     });
   }
 
-  private getRestConfig(options?: { [key: string]: any }): AxiosRequestConfig {
+  public getItem<T>(
+    endpointPath: string,
+    id: number,
+    depth?: Depth,
+  ): Promise<T> {
+    const options = depth ? { depth } : null;
+    return Axios.get(
+      this.makeStandardAbsoluteUrl(endpointPath, id),
+      this.getRestConfig(options),
+    ).then((response) => {
+      return this.readResponse<T>(response);
+    }).catch((error) => {
+      throw error;
+    });
+  }
+
+  private getRestConfig(options?: { [key: string]: any } | null): AxiosRequestConfig {
     const config: AxiosRequestConfig = {
       auth: {
         password: this.credentials.password,
@@ -39,8 +55,12 @@ export class RestClient {
     return config;
   }
 
-  private makeStandardAbsoluteUrl(apiPath: string) {
-    return `${this.baseUrls.urls.base}${apiPath}`;
+  private makeStandardAbsoluteUrl(apiPath: string, id?: number) {
+    if (id) {
+      return `${this.baseUrls.urls.base}${apiPath}/${id}`;
+    } else {
+      return `${this.baseUrls.urls.base}${apiPath}`;
+    }
   }
 
   private readResponse<T>(response: AxiosResponse) {
