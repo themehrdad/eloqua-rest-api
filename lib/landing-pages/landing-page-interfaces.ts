2cb4d625b207fdea8f2ae8876d867c4d4ab4ea4e
Mehrdad Youssefi <themehrdad@gmail.com>
2019-12-16 02:29:48 -0800

    added interfaces

--- lib/landing-pages/landing-page-interfaces.ts
+++ lib/landing-pages/landing-page-interfaces.ts
@@ -1,6 +1,11 @@
+import { IForm } from "../forms/forms-interface";
 import { Depth } from "../rest-api-interfaces";
 import { IContentSection } from "../shared/content-section-interface";
+import { IDynamicContent } from "../shared/dynamic-content-interfaces";
+import { IHtmlContent } from "../shared/html-content-interface";
 import { IHyperlink } from "../shared/hyperlink-interface";
+import { IImage } from "../shared/image-interface";
+import { IImportedFile } from "../shared/imported-file-interface";
 
 export interface ILandingPage {
   id: number;
@@ -8,4 +13,26 @@ export interface ILandingPage {
   autoCloseWaitFor?: string;
   autoRedirectWaitFor?: string;
   contentSections?: IContentSection[];
+  createdAt?: string;
+  createdBy?: string;
+  currentStatus?: string;
+  deployedAt?: string;
+  depth?: Depth;
+  description?: string;
+  dynamicContents?: IDynamicContent[];
+  files?: IImportedFile[];
+  forms?: IForm[];
+  htmlContent?: IHtmlContent;
+  hyperlinks?: IHyperlink[];
+  images?: IImage[];
+  layout?: string;
+  micrositeId?: string;
+  name?: string;
+  permissions?: string;
+  refreshedAt?: string;
+  relativePath?: string;
+  style?: string;
+  type?: string;
+  updatedAt?: string;
+  updatedBy?: string;
 }
---
+++ lib/shared/html-content-interface.ts
@@ -0,0 +1,3 @@
+export interface IHtmlContent {
+  type?: string;
+}
