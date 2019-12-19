2d8fd31e9a9b4823d71c1eb2428dc27cde701a1c
Mehrdad Youssefi <themehrdad@gmail.com>
2019-12-19 05:42:29 -0800

    emails client added

---
+++ lib/shared/field-merge-interface.ts
@@ -0,0 +1,22 @@
+export interface IFieldMerge {
+  accessedAt?: string;
+  accountFieldId?: string;
+  contactFieldId?: string;
+  createdAt?: string;
+  createdBy?: string;
+  currentStatus?: string;
+  defaultValue?: string;
+  depth?: string;
+  description?: string;
+  eventFieldId?: string;
+  eventId?: string;
+  eventSessionFieldId?: string;
+  id?: string;
+  mergeType?: string;
+  name?: string;
+  permissions?: string;
+  syntax?: string;
+  type?: string;
+  updatedAt?: string;
+  updatedBy?: string;
+}
