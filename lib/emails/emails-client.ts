0da24550b248f8007c6bdde2412c9f8eec5f13c0
Mehrdad Youssefi <themehrdad@gmail.com>
2019-12-22 21:48:29 -0800

    url fix

--- lib/emails/emails-client.ts
+++ lib/emails/emails-client.ts
@@ -2,8 +2,8 @@ import { Depth, IListRequestOptions, IListResponse } from "..";
 import { RestClient } from "../rest-client";
 import { IEmail } from "./emails-interfaces";
 
-const getListPath = "/api/REST/1.0/assets/landingPages";
-const getItemPath = "/api/REST/1.0/assets/landingPage";
+const getListPath = "/api/REST/1.0/assets/emails";
+const getItemPath = "/api/REST/1.0/assets/email";
 
 export class EmailClient {
   constructor(private client: RestClient) { }
